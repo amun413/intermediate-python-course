e6d22d9c4397c7f984b831f4c79744279758eee8
Anna M <mun.a@husky.neu.edu>
2020-04-15 21:25:00 +0900

    I added more players. even score bug

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,25 @@
 def main():
     import random
+    number_of_players = int(input('How many players are playing? '))
     dice_rolls = int(input('How many dice would you like to roll? '))
     dice_size = int(input('How many sides are the dice? '))
-    dice_sum = 0
-    for i in range (0,dice_rolls):
-        roll = random.randint(1,dice_size)
-        dice_sum = dice_sum + roll
-        if roll == 1:
-            print(f'You rolled a {roll}! Critical Fail')
-        elif roll == dice_size:
-            print(f'You rolled a {roll}! Critical Success!')
-        else:
-            print(f'You rolled a {roll}')
-    print(f'You have rolled a total of {dice_sum}')
+    score_list = []
+    for j in range(0,number_of_players):
+        dice_sum = 0
+        for i in range (0,dice_rolls):
+            roll = random.randint(1,dice_size)
+            dice_sum = dice_sum + roll
+            if roll == 1:
+                print(f'Player{j} rolled a {roll}! Critical Fail')
+            elif roll == dice_size:
+                print(f'Player{j} rolled a {roll}! Critical Success!')
+            else:
+                print(f'Player{j} rolled a {roll}')
+                print(f'Player{j} have rolled a total of {dice_sum}')
+        score_list.append(j+1)
+        score_list.append(dice_sum)
+    max_roll = max(score_list)
+    print(f'Final score: {score_list}, Winning score is: {max_roll}, Winner is Player{score_list[score_list.index(max_roll)-1]}')
 
 if __name__== "__main__":
     main()
